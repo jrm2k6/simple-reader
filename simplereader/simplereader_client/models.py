ccd46f902e4a4e359ee240eb789c544841caa8bd
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-04-29 23:31:35 +0200

    [models] Create model Feed and User

--- simplereader/simplereader_client/models.py
+++ simplereader/simplereader_client/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=300)
+    url = models.URLField(max_length=500)
+    date_added = models.DateField(True)
+    category = models.CharField(max_length=300)
+
+    def __unicode__(self):
+        return self.title + ' ::' + str(self.url)
+
+class User(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name + ' ::' + str(self.email)
