fbc63f425f05d98503dd62d1649f16fbe0e5e3c9
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-05-01 20:44:52 +0200

    [authentication] Hide Sign Up button when displaying signup form.

--- simplereader/simplereader_client/static/simplereader_client/js/angular/authentication.js
+++ simplereader/simplereader_client/static/simplereader_client/js/angular/authentication.js
@@ -1,10 +1,13 @@
 function AuthenticationController($scope)
 {
+    $scope.signupFieldHidden = true;
+
     $scope.logIn = function()
     {
     };
 
     $scope.signUp = function()
     {
+        $scope.signupFieldHidden = false;
     };
 }
