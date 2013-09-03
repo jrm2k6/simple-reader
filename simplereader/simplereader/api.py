cbeafda0b747a7560cbce83ea658b21fe426d949
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-09-03 23:32:18 +0200

    [api] Add endpoints for newuser, addfeed

--- simplereader/simplereader/api.py
+++ simplereader/simplereader/api.py
@@ -1,13 +1,59 @@
 from tastypie.resources import ModelResource
 from simplereader_client.models import ReaderUser, Feed
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from tastypie.authentication import (
+    Authentication, ApiKeyAuthentication, BasicAuthentication,
+    MultiAuthentication)
+from tastypie.authorization import Authorization
+from tastypie import fields
+
+
+class CreateReaderUserResource(ModelResource):
+    user = fields.OneToOneField('UserResource', 'user', full=False)
+    
+    class Meta:
+        allowed_methods = ['post']
+        always_return_data = True
+        authentication = Authentication()
+        authorization = Authorization()
+        queryset = ReaderUser.objects.all()
+        resource_name = 'newuser'
+
 
 class ReaderUserResource(ModelResource):
     class Meta:
         queryset = ReaderUser.objects.all()
+        allowed_methods = ['get, put, patch']
         resource_name = 'ruser'
+
+
+class UserResource(ModelResource):
+    raw_password = fields.CharField(attribute=None, readonly=True, null=True,
+                                    blank=True)
+ 
+    class Meta:
+        authentication = MultiAuthentication(
+            BasicAuthentication(),
+            ApiKeyAuthentication())
+        authorization = Authorization()
+ 
+        allowed_methods = ['get', 'patch', 'put', ]
+        always_return_data = True
+        queryset = User.objects.all().select_related("api_key")
 
 
 class FeedResource(ModelResource):
     class Meta:
         queryset = Feed.objects.all()
         resource_name = 'feed'
+
+
+class CreateFeedResource(ModelResource):
+    class Meta:
+        allowed_methods = ['post']
+        always_return_data = True
+        authentication = Authentication()
+        authorization = Authorization()
+        queryset = Feed.objects.all()
+        resource_name = 'addfeed'
--- simplereader/simplereader/urls.py
+++ simplereader/simplereader/urls.py
@@ -3,17 +3,23 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
-from simplereader.api import ReaderUserResource, FeedResource
+from simplereader.api import (
+    ReaderUserResource, FeedResource, 
+    CreateReaderUserResource, CreateFeedResource)
 
 user_resource = ReaderUserResource()
+create_user_resource = CreateReaderUserResource()
 feed_resource = FeedResource()
+create_feed_resource = CreateFeedResource()
 
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'simplereader_client.views.home', name='home'),
      url(r'^auth/', 'simplereader_client.views.auth', name='auth'),
      url(r'^api/', include(user_resource.urls)),
+     url(r'^api/', include(create_user_resource.urls)),
      url(r'^api/', include(feed_resource.urls)),
+     url(r'^api/', include(create_feed_resource.urls)),
     # url(r'^simplereader/', include('simplereader.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
