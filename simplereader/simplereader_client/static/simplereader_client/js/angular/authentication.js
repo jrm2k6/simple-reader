b65bf8e7d6c93c85491eb2fcdf3ea599fc2adff3
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-09-04 23:14:58 +0200

    [authentication] Create new users + confirmation message

--- simplereader/simplereader_client/static/simplereader_client/js/angular/authentication.js
+++ simplereader/simplereader_client/static/simplereader_client/js/angular/authentication.js
@@ -1,7 +1,8 @@
 var app = angular.module('angSimpleReader', []);
 
-function AuthenticationController($scope) {
+function AuthenticationController($scope, $http) {
     $scope.signupFieldHidden = true;
+    $scope.creationDone = false;
 
     $scope.logIn = function() {
     };
@@ -10,8 +11,26 @@ function AuthenticationController($scope) {
         $scope.signupFieldHidden = false;
     };
 
-    $scope.createAccount = function(email, password) {
+    $scope.createAccount = function(_email, _password) {
+        $http({
+            url: 'http://localhost:8000/api/newuser/',
+            method: 'POST',
+            data: JSON.stringify({first_name: "", last_name: "", email: _email, password: _password}),
+            headers: {'Content-Type': 'application/json'},
+	     }).
+	    success(function(data, status) {  $scope.showConfirmationMessage(true);}).
+	    error(function(data, status) { $scope.showConfirmationMessage(false);})
     };
+
+    $scope.showConfirmationMessage = function(isASuccess) {
+        $scope.creationDone = true;
+        $scope.userCreatedSuccess = isASuccess? "Woohoo, your user have been created!" : "Oops, something happened, please, try again!";
+        $scope.classMessage = isASuccess? "alert-success" :"alert-error";
+    };           
+
+
+
+
 }
 
 
