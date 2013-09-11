b3cc35343eecfa7da587effdbb92d0cc25886ed6
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-09-11 23:59:28 +0200

    [templates] remove useless template and render authentication template.

--- simplereader/simplereader_client/views.py
+++ simplereader/simplereader_client/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from simplereader_client.admin import ReaderUserCreationForm
+from simplereader_client.forms import ReaderUserCreationForm
 from simplereader_client.models import ReaderUser
 
 
@@ -8,12 +8,11 @@ def home(request):
     return render(request, 'home.html', {'text': text})
 
 def auth(request):
-    return render(request, 'authentication_bis.html', {'form': ReaderUserCreationForm()})
+    return render(request, 'authentication.html', {'form': ReaderUserCreationForm()})
 
 def signup(request):
     if request.method == 'POST':
         form = ReaderUserCreationForm(request.POST)
         if form.is_valid():
-            print form.cleaned_data
             new_user = form.save()
-    return render(request, 'authentication_bis.html', {'form': ReaderUserCreationForm()})
+    return render(request, 'authentication.html', {'form': ReaderUserCreationForm()})
