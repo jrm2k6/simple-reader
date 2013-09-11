6d14f887615a0f0d5a62b61a44c515a42d874652
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-09-11 23:58:37 +0200

    [forms] Move forms from admin to dedicated forms file

--- simplereader/simplereader_client/admin.py
+++ simplereader/simplereader_client/admin.py
@@ -1,55 +1,9 @@
-from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from simplereader_client.models import Feed, ReaderUser
-
-
-class ReaderUserCreationForm(forms.ModelForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm your password', widget=forms.PasswordInput)
-
-    class Meta:
-        model = ReaderUser
-        fields = ('email', 'first_name', 'last_name')
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-   
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(ReaderUserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class ReaderUserChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = ReaderUser
-        fields = ['email', 'password', 'first_name', 'last_name', 'is_active', 'is_admin']
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
+from simplereader_client.forms import ReaderUserChangeForm, ReaderUserCreationForm
 
 
 class ReaderUserAdmin(UserAdmin):
@@ -79,10 +33,8 @@ class ReaderUserAdmin(UserAdmin):
     ordering = ('email',)
     filter_horizontal = ()
 
+
 admin.site.register(ReaderUser, ReaderUserAdmin)
 admin.site.unregister(Group)
 
 admin.site.register(Feed)
-#admin.site.register(ReaderUser)
-#admin.site.unregister(User)
-#admin.site.register(User, UserAdmin)
