c385826395b6f2463f50554c36f0bc77199ad9cd
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-09-13 00:24:24 +0200

    [authentication] Users can sign in.

--- simplereader/simplereader_client/views.py
+++ simplereader/simplereader_client/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from simplereader_client.forms import ReaderUserCreationForm
 from simplereader_client.models import ReaderUser
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def home(request):
@@ -10,9 +13,26 @@ def home(request):
 def auth(request):
     return render(request, 'authentication.html', {'form': ReaderUserCreationForm()})
 
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            new_user = authenticate(email=request.POST['username'], password=request.POST['password'])
+            login(request, new_user)
+            return HttpResponseRedirect("/")
+        else:
+            form = AuthenticationForm()
+    else:
+        form = AuthenticationForm()
+    return render(request, 'authentication.html', {'form': ReaderUserCreationForm(), 'signinForm': form})
+    
+
 def signup(request):
     if request.method == 'POST':
         form = ReaderUserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-    return render(request, 'authentication.html', {'form': ReaderUserCreationForm()})
+            new_user = authenticate(email=request.POST['email'], password=request.POST['password1'])
+            login(request, new_user)
+            return HttpResponseRedirect("/")
+    return render(request, 'authentication.html', {'form': ReaderUserCreationForm(), 'signinForm': ReaderUserSignInForm()})
